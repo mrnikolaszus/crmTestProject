4eb2b1aa1be705813b0fe3ff72a6e7ad658125c1
Nikolas <9225407@gmail.com>
2023-12-18 18:49:54 +0300

    4d task done Использовал библиотеку TestContainers, запуск docker контейнера для тестов postgres происходит динамически, и контейнер гасится после тестов. Реализация H2 так же сделана в другой ветке, но из-за особенностей H2 переделал так.

--- src/test/java/com/javarush/jira/AbstractControllerTest.java
+++ src/test/java/com/javarush/jira/AbstractControllerTest.java
@@ -7,12 +7,14 @@ import org.springframework.test.context.jdbc.SqlConfig;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.testcontainers.containers.PostgreSQLContainer;
 
 //https://docs.spring.io/spring-boot/docs/current/reference/html/spring-boot-features.html#boot-features-testing-spring-boot-applications
 @Sql(scripts = {"classpath:db/changelog.sql", "classpath:data.sql"}, config = @SqlConfig(encoding = "UTF-8"))
 @AutoConfigureMockMvc
 //https://docs.spring.io/spring-boot/docs/current/reference/html/spring-boot-features.html#boot-features-testing-spring-boot-applications-testing-with-mock-environment
 public abstract class AbstractControllerTest extends BaseTests {
+
 
     @Autowired
     private MockMvc mockMvc;
--- src/test/java/com/javarush/jira/BaseTests.java
+++ src/test/java/com/javarush/jira/BaseTests.java
@@ -1,9 +1,43 @@
 package com.javarush.jira;
 
+import com.github.dockerjava.api.model.ExposedPort;
+import com.github.dockerjava.api.model.HostConfig;
+import com.github.dockerjava.api.model.PortBinding;
+import com.github.dockerjava.api.model.Ports;
+import org.junit.jupiter.api.BeforeAll;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Bean;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.utility.DockerImageName;
+
+import java.util.Arrays;
 
 @SpringBootTest
 @ActiveProfiles("test")
 abstract class BaseTests {
+
+    private static final int CONTAINER_PORT = 5433;
+    private static final int LOCAL_PORT = 5432;
+    private static final PostgreSQLContainer<?> container = new PostgreSQLContainer<>("postgres:14.3");
+
+    @BeforeAll
+    static void runContainer() {
+                 container
+                .withExposedPorts(LOCAL_PORT)
+                .withCreateContainerCmdModifier(cmd -> cmd.withHostConfig(
+                        new HostConfig().withPortBindings(new PortBinding(Ports.Binding.bindPort(CONTAINER_PORT), new ExposedPort(LOCAL_PORT)))
+                ));
+        container.start();
+    }
+
+    @DynamicPropertySource
+    static void postgresProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", container::getJdbcUrl);
+
+    }
 }
+
+
